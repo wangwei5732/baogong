0b71aa9fe5ffe279b746291c2978de860eec5d29
wangwei <1310692942@qq.com>
2019-08-13 19:40:09 +0800

    报工1.0上线了

--- src/main/java/BaoGong.java
+++ src/main/java/BaoGong.java
@@ -1,14 +1,16 @@
 import com.baidu.aip.ocr.AipOcr;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
-import org.apache.commons.lang3.time.DateUtils;
 import org.json.JSONObject;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.json.Json;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -18,27 +20,24 @@ import java.util.*;
  */
 public class BaoGong {
     //  登录页
-    private static String logonAction="http://sapmb01.hollysys.net:5280/OnDemand/logonAction.action";
+    private static String logonAction = "http://sapmb01.hollysys.net:5280/OnDemand/logonAction.action";
     //  报工页面
     //  新建补录页面
-    private static String qeruyworkTimeReportAction="http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/qeruyworkTimeReportAction.action";
-    private static String createReportActionReportAction="http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/createReportAction.action?jobworkid=";
+    private static String qeruyworkTimeReportAction = "http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/qeruyworkTimeReportAction.action";
+    private static String createReportActionReportAction = "http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/createReportAction.action?jobworkid=";
     // 报工填写页面
-    private static String updWorkReportAction="http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/updWorkReportAction.action?ser=";
+    private static String updWorkReportAction = "http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/updWorkReportAction.action?ser=";
 
     public static void main(String[] args) {
-        //    账号
-        String userName="184255";
-        //    密码
-        String password="";
+        BaoGongConfig baoGongConfig = BaoGong.loadConfig();
         //    角色
-        RoleEnum role=RoleEnum.GCS;
+        RoleEnum role = RoleEnum.GCS;
 
         //配置webdriver
         System.setProperty("webdriver.chrome.driver", BaoGong.getChromeDriverUrl());
         WebDriver webDriver = new ChromeDriver();
-        webDriver.manage().window().setSize(new Dimension(1024,768));
-        Scanner scanner  = new Scanner(System.in);
+        webDriver.manage().window().setSize(new Dimension(1024, 768));
+        Scanner scanner = new Scanner(System.in);
         try {
 
             //1.进入网站并模拟登陆
@@ -49,21 +48,11 @@ public class BaoGong {
             // 获取验证码
             String validateStr = BaoGong.getValidateCode(webDriver);
 
-            while (StringUtils.isEmpty(userName)||StringUtils.isEmpty(password)){
-                //用户名密码输入
-                if (StringUtils.isEmpty(userName)){
-                    System.out.println("请输入用户名:");
-                    userName=scanner.next();
-                }else{
-                    System.out.println("请输入密码:");
-                    password=scanner.next();
-                }
-            }
             //开始模拟登陆
-            webDriver.findElement(By.name("txtEmpId")).sendKeys(userName);
-            String changeRoleScript = "document.loginform.txtRole.value =\""+role.getRoleVale()+"\"";
+            webDriver.findElement(By.name("txtEmpId")).sendKeys(baoGongConfig.getUsername());
+            String changeRoleScript = "document.loginform.txtRole.value =\"" + role.getRoleVale() + "\"";
             ((ChromeDriver) webDriver).executeScript(changeRoleScript);
-            webDriver.findElement(By.name("txtPwd")).sendKeys(password);
+            webDriver.findElement(By.name("txtPwd")).sendKeys(baoGongConfig.getPassword());
             webDriver.findElement(By.name("txtRand")).sendKeys(validateStr);
             ((ChromeDriver) webDriver).findElementById("btSubmit").click();
 
@@ -72,18 +61,18 @@ public class BaoGong {
             //已报工列表查询页
             String listPageUrl = "";
             //报工填写时需要用
-            String workName ="";
+            String workName = "";
             String workId = "";
             //循环遍历出当前时间的任务
             List<WebElement> elements = webDriver.findElements(By.className("pEven"));
-            int nowDate = Integer.valueOf(DateFormatUtils.format(new Date(),"yyyyMMdd"));
-            for (int i= elements.size()-1 ;i>=0 ;i-- ) {
+            int nowDate = Integer.valueOf(DateFormatUtils.format(new Date(), "yyyyMMdd"));
+            for (int i = elements.size() - 1; i >= 0; i--) {
                 WebElement ele = elements.get(i);
                 String startDateStr = ele.findElement(By.xpath("td[10]/div")).getAttribute("title");
                 String endDateStr = ele.findElement(By.xpath("td[11]/div")).getAttribute("title");
-                int startDate =Integer.valueOf(startDateStr.replace("-",""));
-                int endDate =Integer.valueOf(endDateStr.replace("-",""));
-                if(nowDate<=endDate && nowDate>=startDate){
+                int startDate = Integer.valueOf(startDateStr.replace("-", ""));
+                int endDate = Integer.valueOf(endDateStr.replace("-", ""));
+                if (nowDate <= endDate && nowDate >= startDate) {
                     WebElement firstRadioBox = ele.findElement(By.id("radBox"));
                     //内容：singleRadBox(this,'500024284202101-184255','184255','R-D791803MS5000242842','1');
                     String[] firstRadioParam = firstRadioBox.getAttribute("onclick").split(",");
@@ -93,28 +82,25 @@ public class BaoGong {
                     String txtCustId = firstRadioParam[3];
                     String checkstuta = firstRadioParam[4].split("\\)")[0];
                     //组装赋值script
-                    String workListPageUrlScript = "return 'http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/updWorkReportAction.action?ser=' + encodeURI(encodeURI(getDate()))+'&jobworkid='+encodeURI(encodeURI("+workId
-                            +"))+'&paEmpCode='+"+empCode+"+'&txtCustId='+"+txtCustId+"+'&checkstuta='+"+checkstuta;
+                    String workListPageUrlScript = "return 'http://sapmb01.hollysys.net:5280/OnDemand/wrkmng/bcmng/updWorkReportAction.action?ser=' + encodeURI(encodeURI(getDate()))+'&jobworkid='+encodeURI(encodeURI(" + workId
+                            + "))+'&paEmpCode='+" + empCode + "+'&txtCustId='+" + txtCustId + "+'&checkstuta='+" + checkstuta;
                     listPageUrl = ((ChromeDriver) webDriver).executeScript(workListPageUrlScript).toString();
                 }
             }
             //3.去工时查询页（填报工时必须去这个页面一次，否则报错）
             webDriver.navigate().to(listPageUrl);
-            System.out.println("是否自动填写工时：（1：是 2 否）");
-            if(!"1".equals(scanner.next())){
-                throw new Exception("中断");
-            }
             //4进入新建补录页面
-            webDriver.navigate().to(createReportActionReportAction+workId.replace("'",""));
+            webDriver.navigate().to(createReportActionReportAction + workId.replace("'", ""));
 //            String txtQPlanEdt = LocalDate.now().toString();
-            String txtQPlanEdt = "";
+            String txtQPlanEdt = "" + nowDate;
             String txtWorknumber = "1";
             String txtContextDescription = workName;
 
 
-            ((ChromeDriver) webDriver).findElementById("txtQPlanEdt").sendKeys(txtQPlanEdt);
+            String changeDateScript = "document.getElementById(\"txtQPlanEdt\").value='"+baoGongConfig.getDate()+"'";
+            ((ChromeDriver) webDriver).executeScript(changeDateScript);
             ((ChromeDriver) webDriver).findElementById("txtWorknumber").sendKeys(txtWorknumber);
-            ((ChromeDriver) webDriver).findElementById("txtContextDescription").sendKeys(txtContextDescription);
+            ((ChromeDriver) webDriver).findElementById("txtContextDescription").sendKeys(baoGongConfig.getDescription());
             WebElement commitBtn = ((ChromeDriver) webDriver).findElement(By.xpath("//*[@id=\"createReportAction\"]/table[1]/tbody/tr[2]/td[2]/span[1]"));
             commitBtn.click();
 
@@ -123,36 +109,36 @@ public class BaoGong {
             alert2.accept();
 
             webDriver.navigate().to(listPageUrl);
-            System.out.println("输入任意值，并回车结束：");
+            System.out.println("输入1，并回车结束：");
             scanner.next();
             System.out.println("结束");
 
-        }catch (UnhandledAlertException e){
-            if(e.getMessage().indexOf("验证码输入有误,请重新输入")>0){
+        } catch (UnhandledAlertException e) {
+            if (e.getMessage().indexOf("验证码输入有误,请重新输入") > 0) {
                 System.out.println("验证码识别错误，请重新运行代码！");
             }
-            if("新增补录成功".equals(e.getAlertText())){
+            if ("新增补录成功".equals(e.getAlertText())) {
                 System.out.println("新增补录成功!");
             }
 
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             webDriver.quit();
             scanner.close();
         }
 
     }
+
     /**
+     * @return java.lang.String
      * @Author wangwei
      * @Description //TODO 获取图片验证码
      * @Date 18:01 2019-07-21
      * @Param []
-     * @return java.lang.String
      **/
-    public static String getValidateStr(){
-        String validateStr ="";
+    public static String getValidateStr() {
+        String validateStr = "";
         try {
             //百度图像识别获取验证码
             String APP_ID = "16853191";
@@ -164,7 +150,7 @@ public class BaoGong {
             String path = "validateImg/validateImg.png";
             JSONObject res = client.basicGeneral(path, new HashMap<String, String>());
             validateStr = res.getJSONArray("words_result").getJSONObject(0).getString("words");
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return validateStr;
@@ -172,41 +158,42 @@ public class BaoGong {
 
     /**
      * 根据系统类型获取chromedriver
+     *
      * @return chromedriver Url
      */
-    private static String getChromeDriverUrl(){
+    private static String getChromeDriverUrl() {
         Properties prop = System.getProperties();
         String osName = prop.getProperty("os.name");
+        String url = BaoGong.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        String path = url.substring(0, url.lastIndexOf("/"));
         if (osName.startsWith("Mac OS")) {
             // 苹果
-            return "src/main/resources/chromedriver";
-        } else if (osName.startsWith("Windows")) {
-            // windows
-            return "src/main/resources/chromedriver.exe";
+            return path + "/chromedriver";
         } else {
-            // unix or linux
-            return "src/main/resources/chromedriver";
+            // windows
+            return path + "/chromedriver.exe";
         }
     }
 
     /**
      * 截取验证码，使用百度地图api进行识别，返回验证码
+     *
      * @param webDriver
      * @return 验证码
      * @throws Exception
      */
-    private  static String getValidateCode(WebDriver webDriver) throws Exception{
+    private static String getValidateCode(WebDriver webDriver) throws Exception {
         //获取验证码element
         WebElement validateCode = ((ChromeDriver) webDriver).findElement(By.xpath("//*[@id=\"mainFrame\"]/table[1]/tbody/tr[2]/td[2]/table/tbody/tr[5]/td[2]/span/img"));
         //截取整个网站图片
-        File screenshot = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+        File screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
         BufferedImage fullImage = ImageIO.read(screenshot);
         Point point = validateCode.getLocation();
 
         int width = validateCode.getSize().getWidth();
         int height = validateCode.getSize().getHeight();
-        int imgX =point.getX();
-        int imgY =point.getY();
+        int imgX = point.getX();
+        int imgY = point.getY();
 
         // mark
         Properties prop = System.getProperties();
@@ -215,25 +202,68 @@ public class BaoGong {
             // Mac
             // 获取网站页面尺寸，并计算尺寸比例（网站/图片）
             Dimension dimension = webDriver.manage().window().getSize();
-            double scaleW = (double) dimension.getWidth()/fullImage.getWidth();
+            double scaleW = (double) dimension.getWidth() / fullImage.getWidth();
             double scaleH = scaleW;
-            width = (int)(validateCode.getSize().getWidth()/scaleW) ;
-            height = (int) (validateCode.getSize().getHeight()/scaleH);
-            imgX =(int)(point.getX()/scaleW);
-            imgY =(int)(point.getY()/scaleH);
+            width = (int) (validateCode.getSize().getWidth() / scaleW);
+            height = (int) (validateCode.getSize().getHeight() / scaleH);
+            imgX = (int) (point.getX() / scaleW);
+            imgY = (int) (point.getY() / scaleH);
         }
-        BufferedImage validateImg = fullImage.getSubimage(imgX,imgY,width,height);
+        BufferedImage validateImg = fullImage.getSubimage(imgX, imgY, width, height);
 
-        File file = new File("validateImg","validateImg.png");
-        if (!file.exists()){
+        File file = new File("validateImg", "validateImg.png");
+        if (!file.exists()) {
             file.getParentFile().mkdir();
         }
-        ImageIO.write(validateImg,"png",file);
+        ImageIO.write(validateImg, "png", file);
         //获取验证码，添加容错，调用两次
         String validateStr = BaoGong.getValidateStr();
-        if(StringUtils.isEmpty(validateStr)||validateStr.length()!=4){
+        if (StringUtils.isEmpty(validateStr) || validateStr.length() != 4) {
             validateStr = BaoGong.getValidateStr();
         }
         return validateStr;
+    }
+
+    /**
+     * 加载参数
+     *
+     * @return
+     */
+    private static BaoGongConfig loadConfig() {
+        String url = BaoGong.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        String path = url.substring(0, url.lastIndexOf("/"));
+        String baogongStr = BaoGong.readTxt(path + "/baogong.txt");
+        BaoGongConfig baoGongConfig = new Gson().fromJson(baogongStr, BaoGongConfig.class);
+        return baoGongConfig;
+    }
+
+    /**
+     * 读取文本
+     *
+     * @param filePath
+     * @return
+     */
+    public static String readTxt(String filePath) {
+        String txt = "";
+        try {
+            File file = new File(filePath);
+            if (file.isFile() && file.exists()) {
+                String lineTxt = "";
+                InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "utf-8");
+                BufferedReader br = new BufferedReader(isr);
+                while ((lineTxt = br.readLine()) != null) {
+                    System.out.println(lineTxt);
+                    txt += lineTxt;
+                }
+                br.close();
+            } else {
+                System.out.println("文件不存在!");
+            }
+        } catch (Exception e) {
+            System.out.println("文件读取错误!");
+        } finally {
+            return txt;
+        }
+
     }
 }
